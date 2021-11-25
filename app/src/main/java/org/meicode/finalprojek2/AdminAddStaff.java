3856b26ebc3cc0efc532694c82cad489ecafde4b
harrychandra99 <harry.535170076@stu.untar.ac.id>
2021-11-25 10:35:06 +0700

    Update

---
+++ app/src/main/java/org/meicode/finalprojek2/AdminAddStaff.java
@@ -0,0 +1,14 @@
+package org.meicode.finalprojek2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdminAddStaff extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_add_staff);
+    }
+}
